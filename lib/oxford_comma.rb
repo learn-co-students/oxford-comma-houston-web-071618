32b897868bffdc613773320dd299cc3cc1215679
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-06-27 21:58:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join(",")
+  elsif array.length ==2
+    array.join(" and ")
+  else
+    blah = ", and #{array.pop()}"
+    new_array=array.join(", ")
+    new_array.concat(blah)
+  end
 end
