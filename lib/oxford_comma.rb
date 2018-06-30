3c58ed19cd027c20baf5fc577d0ad6ad08200b85
Scott Tang <scott.q.tang@gmail.com>
2018-06-30 18:54:44 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
 
+    formatted_array = Array.new
+
+    if array.length == 1
+        formatted_array = array.join()
+    elsif array.length == 2
+        formatted_array = array.join(" and ")
+    else
+        last_array = array.pop()
+        formatted_array = array.join(", ") + ", and " + last_array
+    end
+    formatted_array
+
 end
+
+
+
+
+puts oxford_comma(["scott"])
+
+
